e8ef79838df97192ff9c511ccf62c5f0ff885b94
Nagoydede <nagoydede@yahoo.com>
2020-07-10 00:12:11 +0900

    Init directory

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarkSpecs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
